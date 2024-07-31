b5a37ab42a9d0bb52097c513abedd67c1e8cec00
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-31 10:15:08 +0200

    todo setup

---
+++ toDo_App/src/App.jsx
@@ -0,0 +1,11 @@
+import Input from "./components/InputButton";
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-9xl font-extrabold text-center text-slate-300">
+        todos
+      </h1>
+      <Input />
+    </div>
+  );
+}
---
+++ toDo_App/src/components/InputButton.jsx
@@ -0,0 +1,10 @@
+export default function IputButton() {
+  return (
+    <input
+      type="text"
+      placeholder="Add new todo!"
+      className="border-2 border-black  text-center"
+    />
+    //   Enter you to do here!
+  );
+}
